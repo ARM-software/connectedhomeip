1a6ac32547f7ac20d2adec77a3e3c0711e0ddb33
Anna Bridge <anna.bridge@arm.com>
2022-12-21 15:59:06 +0000

    psa: Build Matter CryptoPAL using PSA in Open IoT SDK
    This commit allows the selection of PSA as the cryptographic
    algorithm used when building Matter CryptoPAL with the Open
    IoT SDK.
    
    Other important changes:
    
    src/crypto/crypto.gni has been updated to remove the assert
    added by project-chip#23538.
    This assert assumes that you only build mbedtls externally if you
    have chip_crypto = 'mbedtls'. This is an invalid assumption and
    is certainly not the case for the Open IoT SDK.
    
    The GitHub CI workflow for the SDK examples/unit tests has been
    updated to add a matrix test setup which builds and runs the
    examples with both mbedtls and psa cryptographic algorithms.
    
    Add call to psa_crypto_init()
    
    The Matter PSA implementation still uses some underlying MbedTLS
    functions (including random number generation). To use these
    functions however a call to psa_crypto_init() is required.
    
    Signed-off-by: Anna Bridge <anna.bridge@arm.com>

--- config/openiotsdk/cmake/chip.cmake
+++ config/openiotsdk/cmake/chip.cmake
@@ -21,7 +21,7 @@
 
 get_filename_component(GEN_DIR ${CHIP_ROOT}/zzz_generated/ REALPATH)
 
-# Default CHIP build configuration 
+# Default CHIP build configuration
 set(CONFIG_CHIP_PROJECT_CONFIG "main/include/CHIPProjectConfig.h" CACHE STRING "")
 set(CONFIG_CHIP_LIB_TESTS NO CACHE BOOL "")
 set(CONFIG_CHIP_LIB_SHELL NO CACHE BOOL "")
@@ -34,6 +34,7 @@ set(CONFIG_CHIP_ERROR_LOGGING YES CACHE BOOL "Enable logging at error level")
 set(CONFIG_CHIP_OPEN_IOT_SDK_USE_PSA_PS NO CACHE BOOL "Enable using PSA Protected Storage")
 
 set(CONFIG_CHIP_OPEN_IOT_SDK_USE_IOT_SOCKET NO CACHE BOOL "Enable using IoT Socket API")
+set(CONFIG_CHIP_CRYPTO "mbedtls" CACHE STRING "Matter crypto backend. Mbedtls as default")
 
 if(CONFIG_CHIP_OPEN_IOT_SDK_USE_PSA_PS AND NOT TFM_SUPPORT)
     message( FATAL_ERROR "You can not use PSA Protected Storage without TF-M support" )
@@ -52,14 +53,20 @@ if(TFM_SUPPORT)
     endif()
 endif()
 
+if ("${CONFIG_CHIP_CRYPTO}" STREQUAL "psa")
+    target_compile_definitions(openiotsdk-chip
+        INTERFACE
+            CONFIG_CHIP_CRYPTO_PSA)
+endif()
+
 function(chip_add_data_model target model_name)
-    target_include_directories(${target} 
+    target_include_directories(${target}
         PUBLIC
             ${GEN_DIR}/app-common
             ${GEN_DIR}/${model_name}-app
     )
 
-    target_sources(${target} 
+    target_sources(${target}
         PUBLIC
             ${GEN_DIR}/${model_name}-app/zap-generated/IMClusterCommandHandler.cpp
     )
