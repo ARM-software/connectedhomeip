46ae2b750b1d8ec0e1d9e7e3e9fdd3c8c5f530ac
ATmobica <artur.tynecki@arm.com>
2023-02-01 08:40:31 +0000

    [OIS] Add IoT Socket support
    Add OIS IoT Socket to build system.
    Implement IoT socket system layer and endpoints (UDP and TCP).
    Adapting inet and system layers to IoT Socket support.
    Improving unit-tests for IoT socket support.
    Add IoT socket support in OIS PlatformManagerImpl.
    Move initialisation of openiotsdk to after network init.
    
    Signed-off-by: paul-szczepanek-arm <paul.szczepanek@arm.com>

--- config/openiotsdk/cmake/sdk.cmake
+++ config/openiotsdk/cmake/sdk.cmake
@@ -42,6 +42,14 @@ FetchContent_Declare(
     GIT_PROGRESS   ON
 )
 
+FetchContent_Declare(
+    lwip
+    GIT_REPOSITORY  https://github.com/lwip-tcpip/lwip.git
+    GIT_TAG         STABLE-2_1_3_RELEASE
+    GIT_PROGRESS    ON
+    PATCH_COMMAND   git apply --ignore-space-change --ignore-whitespace ${CHIP_ROOT}/config/openiotsdk/lwip/lwip.patch || true
+)
+
 # Open IoT SDK configuration
 set(IOTSDK_FETCH_LIST
     mcu-driver-reference-platforms-for-arm
